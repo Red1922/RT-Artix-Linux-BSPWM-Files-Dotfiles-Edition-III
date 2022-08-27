eb10a7a97d4d46c8bd093b4774780933e08aca6d
R. T. Redreovich <77635620+Red1922@users.noreply.github.com>
2022-08-27 10:38:20 +0530

    Added all files. Enjoy.
    Readme.md will be updated later.

---
+++ Rofi/powermenu.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Options for powermenu
+lock=" Lock"
+#logout=" Logout"
+shutdown=" Shutdown"
+reboot=" Reboot"
+#sleep="....Sleep"
+
+# Get answer from user via rofi
+selected_option=$(echo "$lock
+$shutdown
+$reboot" | rofi -dmenu\
+                  -i\
+                  -p "Power"\
+                  -config "~/.config/rofi/powermenu.rasi"\
+                  -font "Monospace 11"\
+                  -width "15"\
+                  -lines 3\
+                  -line-margin 3\
+                  -line-padding 10\
+                  -scrollbar-width "0" )
+
+# Do something based on selected option
+if [ "$selected_option" == "$lock" ];
+then
+   # /home/$USER/.config/bspwm/scripts/i3lock-fancy/i3lock-fancy.sh
+   betterlockscreen -l dimblur
+#elif [ "$selected_option" == "$logout" ]
+#then
+#    bspc quit
+elif [ "$selected_option" == "$shutdown" ];
+then
+    loginctl poweroff
+elif [ "$selected_option" == "$reboot" ];
+then
+    loginctl reboot
+#elif [ "$selected_option" == "$sleep" ];
+#then
+#    amixer set Master mute
+#    suspend
+else
+    echo "No match"
+fi
